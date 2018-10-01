aba0b9f4be8dd51ac79b4270b22c8cbc44129bde
Jean Lambert <jeeaan1702@gmail.com>
2018-09-30 22:59:34 -0700

    initial config

--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,12 @@
-import pgp from 'pg-promise';
+import _pgp from 'pg-promise';
+import dotenv from 'dotenv';
+
+const result = dotenv.config()
+if (result.error) {
+  throw result.error;
+}
+
+const pgp = _pgp();
 const db = pgp({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
@@ -6,5 +14,6 @@ const db = pgp({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD
 });
+
 
 export default db;
