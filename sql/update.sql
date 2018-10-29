eca637e1428b9fc1aac92fb054f1c3581eecff25
Jean Lambert <jeeaan1702@gmail.com>
2018-10-28 20:17:05 -0700

    queries updated

--- sql/update.sql
+++ sql/update.sql
@@ -2,3 +2,6 @@ ALTER TABLE public.main_device_params ADD amps int NULL;
 ALTER TABLE public.main_device_params ADD voltage int NULL;
 ALTER TABLE public.main_device_params ADD watts int NULL;
 ALTER TABLE public.main_device_params ADD time timestamp NOT NULL;
+
+CREATE UNIQUE INDEX core_license_code_uindex ON public.core_license (code);
+CREATE UNIQUE INDEX main_notification_type_name_uindex ON public.main_notification_type (name);
