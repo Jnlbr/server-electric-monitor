74ee6ba1c277cb5606ae2c6d892c722320888438
Jean Lambert <jeeaan1702@gmail.com>
2018-10-28 20:03:34 -0700

    fake data initializer

--- sql/data/core_device_type.sql
+++ sql/data/core_device_type.sql
@@ -1,1 +1,2 @@
-INSERT INTO core_device_type (name) VALUES ('CURRENT_SENSOR');
+INSERT INTO core_device_type (name) VALUES ('DC_CURRENT_SENSOR')
+INSERT INTO core_device_type (name) VALUES ('AC_CURRENT_SENSOR')
